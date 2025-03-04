e9fe2e4ebefccc73b6e242e69bf65871be644e34
vicecarloans-real <huyst1997@icloud.com>
2025-03-04 18:57:14 -0500

    feat: checkpoints and key by symbol

--- src/main/java/com/example/TradeProcessor.java
+++ src/main/java/com/example/TradeProcessor.java
@@ -3,20 +3,25 @@ package com.example;
 import com.example.model.TradeEvent;
 import com.example.model.EnrichedTrade;
 
-import java.time.Duration;
+import java.text.SimpleDateFormat;
+// import java.time.Duration;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.configuration.MemorySize;
+// import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.connector.file.sink.FileSink;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.core.io.SimpleVersionedSerializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.BucketAssigner;
 import org.apache.flink.streaming.api.functions.sink.filesystem.OutputFileConfig;
-import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.SimpleVersionedStringSerializer;
+// import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -25,7 +30,8 @@ public class TradeProcessor {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         env.setParallelism(3);
-        env.enableCheckpointing(10000);
+        // Checkpointing every minute
+        env.enableCheckpointing(60 * 1000);
         // Create Kafka source
         KafkaSource<String> source = KafkaSource.<String>builder()
                 .setBootstrapServers("kafka:29092")
@@ -49,12 +55,30 @@ public class TradeProcessor {
         // Configure FileSink with rolling policy
         FileSink<String> sink = FileSink
                         .forRowFormat(outputPath, new SimpleStringEncoder<String>("UTF-8"))
+                        .withBucketAssigner(new BucketAssigner<String, String>() {
+                            @Override
+                            public String getBucketId(String element, Context context) {
+                                String symbol = element.split(",")[0];
+                                long timestamp = context.currentProcessingTime();
+                                String date = new SimpleDateFormat("yyyy-MM-dd").format(new java.util.Date(timestamp));
+                                return date + "/" + symbol;
+                            }
+
+                            @Override
+                            public SimpleVersionedSerializer<String> getSerializer() {
+                                return SimpleVersionedStringSerializer.INSTANCE;
+                            }
+                        })
+                        // .withRollingPolicy(
+                        //     DefaultRollingPolicy.builder()
+                        //         .withRolloverInterval(Duration.ofMinutes(15))
+                        //         .withInactivityInterval(Duration.ofMinutes(5))
+                        //         .withMaxPartSize(MemorySize.ofMebiBytes(1024))
+                        //         .build())
                         .withRollingPolicy(
-                            DefaultRollingPolicy.builder()
-                                .withRolloverInterval(Duration.ofMinutes(15))
-                                .withInactivityInterval(Duration.ofMinutes(5))
-                                .withMaxPartSize(MemorySize.ofMebiBytes(1024))
-                                .build())
+                            // In Streaming mode, the rolling policy is triggered by the checkpoint
+                            OnCheckpointRollingPolicy.build()
+                        )
                         .withOutputFileConfig(
                             OutputFileConfig
                             .builder()
@@ -73,7 +97,8 @@ public class TradeProcessor {
         stream.map(json -> {
                 System.out.println("Processing trade: " + json);
                 return mapper.readValue(json, TradeEvent.class);
-            })  
+            })
+            .keyBy(trade -> trade.getSymbol())  
             .map(trade -> {
                 EnrichedTrade enrichedTrade = new EnrichedTrade(trade);
                 System.out.println("Enriched trade: " + enrichedTrade);
